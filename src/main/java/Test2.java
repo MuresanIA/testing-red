2078898e610d351030af6717d881a179724815e5
MuresanIA <muresan.alexandru933@gmail.com>
2021-02-04 13:18:31 +0200

    Testing for red #36

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -192,6 +192,14 @@ public class Test2 {
         clickOnSelectButton.click();
         System.out.println("Test: Click On Select Button Successful!");
 
+        wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"testing_SupplierLicenceExpire\"]/red-platform-datepicker-control")));
+        System.out.println("Test: Expected conditions Testing Supplier Licence Expire Successful!");
+        WebElement agreeSupplierCheckBox = driver.findElement(By.xpath("//*[@id=\"testing_SupplierAcceptTnc\"]/ion-checkbox"));
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView();", agreeSupplierCheckBox);
+        agreeSupplierCheckBox.click();
+        System.out.println("Test: Click On Agree To The Terms CheckBox Successful!");
+
 //        wait = new WebDriverWait(driver, 20);
 //        driver.close();
 
